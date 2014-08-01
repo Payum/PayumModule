c6c5e02fe154ee8bb5ad20470d554b437fa2b7ee
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-08-01 10:30:02 +0000

    cleanup capture controller.

--- src/Payum/PayumModule/Controller/CaptureController.php
+++ src/Payum/PayumModule/Controller/CaptureController.php
@@ -1,9 +1,11 @@
 <?php
 namespace Payum\PayumModule\Controller;
 
-use Payum\Core\Request\RedirectUrlInteractiveRequest;
+use Payum\Core\Request\Http\RedirectUrlInteractiveRequest;
+use Payum\Core\Request\Http\ResponseInteractiveRequest;
+use Payum\Core\Request\InteractiveRequestInterface;
 use Payum\Core\Request\SecuredCaptureRequest;
-use Payum\Core\Request\BinaryMaskStatusRequest;
+use Zend\Http\Response;
 
 class CaptureController extends PayumController
 {
@@ -13,19 +15,24 @@ class CaptureController extends PayumController
 
         $payment = $this->getPayum()->getPayment($token->getPaymentName());
 
-        $status = new BinaryMaskStatusRequest($token);
-        $payment->execute($status);
-        if (false == $status->isNew()) {
-            $this->getResponse()->setStatusCode(400);
-            return;
-        }
-
         try {
-            $capture = new SecuredCaptureRequest($token);
-            $payment->execute($capture);
-        } catch (RedirectUrlInteractiveRequest $interactiveRequest) {
-            $this->redirect()->toUrl($interactiveRequest->getUrl());
-            return;
+            $payment->execute(new SecuredCaptureRequest($token));
+        } catch (InteractiveRequestInterface $interactiveRequest) {
+            if ($interactiveRequest instanceof RedirectUrlInteractiveRequest) {
+                $this->redirect()->toUrl($interactiveRequest->getUrl());
+            }
+
+            if ($interactiveRequest instanceof ResponseInteractiveRequest) {
+                $this->getResponse()->setContent($interactiveRequest->getContent());
+
+                $response = new Response();
+                $response->setStatusCode(200);
+                $response->setContent($interactiveRequest->getContent());
+
+                return $response;
+            }
+
+            throw new \LogicException('Unsupported interactive request', null, $interactiveRequest);
         }
 
         $this->getHttpRequestVerifier()->invalidate($token);
